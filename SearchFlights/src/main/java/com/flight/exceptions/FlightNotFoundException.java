8b5d1a9f9f486886e6ac8f7a276f4a4e208328ae
Preethi Musuku <preethimusuku@gmail.com>
2020-05-07 13:21:19 +0530

    new commit

--- SearchFlights/src/main/java/com/flight/exceptions/FlightNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.flight.exceptions;
-
-@SuppressWarnings("serial")
-public class FlightNotFoundException extends RuntimeException {
-	public FlightNotFoundException(String fromLoc, String toLoc) {
-	        super("flight not found");
-	    }
-}
