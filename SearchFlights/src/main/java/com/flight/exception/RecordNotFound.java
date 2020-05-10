c8ba17bad78c65e8337d114314b58ac9ea54aada
Preethi Musuku <preethimusuku@gmail.com>
2020-05-10 12:35:03 +0530

    with junit

---
+++ SearchFlights/src/main/java/com/flight/exception/RecordNotFound.java
@@ -0,0 +1,11 @@
+package com.flight.exception;
+
+@SuppressWarnings("serial")
+public class RecordNotFound extends  RuntimeException{
+	public RecordNotFound(String errorMsg){
+		super(errorMsg);
+	}
+	public RecordNotFound(String msg,Throwable e){
+        super(msg,e);
+    }
+}
